a771f61a9fa8dae9c85d11e873b9333c393b9f74
Sydney Quach <sydneyq14@gmail.com>
2023-09-22 23:25:54 -0700

    changes today's weather icon based on uv index

--- weather-app.js
+++ weather-app.js
@@ -1,29 +1,23 @@
-// Define a function to handle the data
-function processData(temperatureData, precipitation, precipitation_probability, apparent_temperature, sunrise, sunset, precipitation_probability_max, uv_index_max) {
-  // You can use temperatureData here or perform any other actions
-  console.log('Temperature data:', temperatureData);
-  console.log('precip:', precipitation);
-  console.log('Precip Prob:', precipitation_probability);
-  console.log('apparent temp:', apparent_temperature);
-  console.log('sunrise: ', sunrise);
-  console.log('sunset: ', sunset);
-  console.log('precipitation_probability_max: ', precipitation_probability_max);
-  console.log('uv: ', uv_index_max);
-}
-
-// Define the API URL
 const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=49.2606&longitude=123.246&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_probability_max&timezone=America%2FLos_Angeles';
 
-// Make a GET request using fetch
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+async function fetchDataFromAPI() {
+  try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+          throw new Error('Failed to fetch data from the API');
+      }
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error(error);
+      throw error; 
     }
-    return response.json(); // Parse the response body as JSON
-  })
+}
 
-  .then(data => {
+
+async function main() {
+    const data = await fetchDataFromAPI();
+    
     // Air temperature at 2 meters above ground (°C)
     const temperatureData = data.hourly.temperature_2m;
 
@@ -60,18 +54,40 @@ fetch(apiUrl)
     // Rain from large scale weather systems of the preceding hour in millimeter
     // (mm)
     const rain = data.hourly.rain;
+    
+    //document.getElementById("today-temp").innerHTML = temperatureData[0];
+      
+    
 
-    processData(temperatureData, precipitation, precipitation_probability, apparent_temperature, sunrise, sunset, precipitation_probability_max, uv_index_max);
-  })
-
-  .catch(error => {
-    // Handle errors
-    console.error('There was a problem with the fetch operation:', error);
-  });
+    //console.log(uv_index_max);
+    //[4.65, 3, 4.05, 4.4, 2.65, 2.8, 2.45]
 
 
-  
 
+    const sun_cloud = document.getElementById("cloudy-sun");
+    const cloud = document.getElementById("cloud");
+    const sun = document.getElementById("sunny-sun");
+
+    if (uv_index_max[0] >= 4) {
+      sun_cloud.style.display = "none";
+      cloud.style.display = "none";
+      sun.style.display = "block";
+
+    } else if (uv_index_max[0] >= 3){
+      sun_cloud.style.display = "block";
+      cloud.style.display = "block";
+      sun.style.display = "none";
+    } else {
+      sun_cloud.style.display = "none";
+      cloud.style.display = "block";
+      sun.style.display = "none";
+    }
+
+
+}
+
+// Call the main function to start the process
+main();
 
 
 
